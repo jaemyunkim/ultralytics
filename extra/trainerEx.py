9f8ef57c66de77ef2b39c97c90e778c34da51a14
Jaemyun Kim <jaemyunkim@outlook.com>
2025-06-18 04:35:49 +0000

    fix to load the multiple dataset

--- extra/trainerEx.py
+++ extra/trainerEx.py
@@ -1,4 +1,9 @@
 from ultralytics.engine.trainer import BaseTrainer
+from typing import Dict, List, Tuple, Union
+from .utilsEx import (
+    check_det_dataset, 
+    check_cls_dataset, 
+)
 
 
 class BaseTrainerEx(BaseTrainer):
@@ -24,7 +29,9 @@ class BaseTrainerEx(BaseTrainer):
                         self.args.data = data["yaml_file"]  # for validating 'yolo train data=url.zip' usage
             elif isinstance(self.args.data, list):
                 if self.args.task == "classify":
-                    data = check_cls_dataset(self.args.data)
+                    data = []
+                    for d in self.args.data:
+                        data.append(check_cls_dataset(d))
                 elif self.args.data.rsplit(".", 1)[-1] in {"yaml", "yml"} or self.args.task in {
                     "detect",
                     "segment",
@@ -37,12 +44,16 @@ class BaseTrainerEx(BaseTrainer):
                         data.append(check_det_dataset(d))
                         if "yaml_file" in data[-1]:
                             out_data.append(data[-1]["yaml_file"])  # for validating 'yolo train data=url.zip' usage
-                    self.args.data = data["yaml_file"]  # for validating 'yolo train data=url.zip' usage
+                    self.args.data = out_data  # for validating 'yolo train data=url.zip' usage
         except Exception as e:
             raise RuntimeError(emojis(f"Dataset '{clean_url(self.args.data)}' error ❌ {e}")) from e
         if self.args.single_cls:
             LOGGER.info("Overriding class names with single class.")
-            for d in data:
-                d["names"] = {0: "item"}
-                d["nc"] = 1
+            if isinstance(data, Dict):
+                data["names"] = {0: "item"}
+                data["nc"] = 1
+            elif isinstance(data, list):
+                for d in data:
+                    d["names"] = {0: "item"}
+                    d["nc"] = 1
         return data
